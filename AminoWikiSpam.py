d36eb4170e9b084cb7cdc7f838c1045162b53c8d
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-04-01 22:53:21 +0800

    Update AminoWikiSpam.py

--- AminoWikiSpam.py
+++ AminoWikiSpam.py
@@ -14,20 +14,16 @@ print("█▀█ █░█░█ █ █░▀█ █░█ █░█░█ █ 
 print("▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ░▀░▀░ ▀ ▀░▀▀ ▀ ▀▀░ █▀░ ▀░▀ ▀░░░▀")
 import amino
 client = amino.Client()
-email=input("Email/Почта:")
-password=input("Password/Пароль:")
-for name, id in zip(client.sub_clients().name, client.sub_clients().comId):
-    print(f"{name}: {id}")
-comid = input("Выберите сообщество(id): ")
-client = amino.Client() 
+email = input("Email/Почта:")
+password = input("Password/Пароль:")
 client.login(email=email, password=password)
 print('\nLogged in/Бот зашел!')
-wikilk=input("Type WikiLink/Введите ссылку на статью:")
-wiki=client.get_from_code(wikilk)
-wikiId=wiki.objectId
-comId=wiki.path[1:wiki.path.index('/')]
-subclient=amino.SubClient(comId=comid ,profile=client.profile)
-comment=input("Type a Comment/Введите коммент:")
+wikilk = input("Type WikiLink/Введите ссылку на статью:")
+wiki = client.get_from_code(wikilk)
+wikiId = wiki.objectId
+comId = wiki.path[1:wiki.path.index('/')]
+subclient = amino.SubClient(comId=comid ,profile=client.profile)
+comment = input("Type a Comment/Введите коммент:")
 while True:
 	try:
 		sub_client.comment(message=comment, wikiId=wikiId)
