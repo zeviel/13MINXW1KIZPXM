3dd9f9ab603ef2400b8a50fba73361883e4f21d1
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-06-10 13:56:59 +0600

    Update AminoWikiSpam.py

--- AminoWikiSpam.py
+++ AminoWikiSpam.py
@@ -1,25 +1,21 @@
-from colorama import init
-from colorama import Fore, Back, Style
+import amino
+import pyfiglet
+from colorama import init, Fore, Back, Style
 init()
-print(Back.BLACK)
 print(Fore.GREEN)
 print("""Script by Lil Zevi
-Github : https://github.com/LilZevi
-▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ █░░░█ ▀ █░▄▀ ▀ ▄▀▀ █▀▄ ▄▀▄ █▄░▄█
-█▀█ █░█░█ █ █░▀█ █░█ █░█░█ █ █▀▄░ █ ░▀▄ █░█ █▀█ █░█░█
-▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ░▀░▀░ ▀ ▀░▀▀ ▀ ▀▀░ █▀░ ▀░▀ ▀░░░▀""")
-import amino
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("aminowikispam", font="shadow"))
 client = amino.Client()
-email = input("Email/Почта:")
-password = input("Password/Пароль:")
+email = input("Email/Почта: ")
+password = input("Password/Пароль: ")
 client.login(email=email, password=password)
-print('\nLogged in/Бот зашел!')
-wikilk = input("Type WikiLink/Введите ссылку на статью:")
+wikilk = input("Type WikiLink/Введите ссылку на статью: ")
 wiki = client.get_from_code(wikilk)
 wikiId = wiki.objectId
 comId = wiki.path[1:wiki.path.index('/')]
 subclient = amino.SubClient(comId=comid ,profile=client.profile)
-comment = input("Type a Comment/Введите коммент:")
+comment = input("Type a Comment/Введите коммент: ")
 while True:
 	try:
 		sub_client.comment(message=comment, wikiId=wikiId)
