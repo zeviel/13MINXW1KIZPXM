f2125f91d1b2b726cddb0a49f388ba5c2bdd968c
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-08-14 01:14:01 +0600

    Update AminoWikiSpam.py

--- AminoWikiSpam.py
+++ AminoWikiSpam.py
@@ -1,4 +1,5 @@
 import amino
+import asyncio
 import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
@@ -6,19 +7,23 @@ print(Fore.GREEN)
 print("""Script by Lil Zevi
 Github : https://github.com/LilZevi""")
 print(pyfiglet.figlet_format("aminowikispam", font="shadow"))
-client = amino.Client()
-email = input("Email/Почта: ")
-password = input("Password/Пароль: ")
-client.login(email=email, password=password)
-wikilk = input("Type WikiLink/Введите ссылку на статью: ")
-wiki = client.get_from_code(wikilk)
-wikiId = wiki.objectId
-comId = wiki.path[1:wiki.path.index('/')]
-subclient = amino.SubClient(comId=comid ,profile=client.profile)
-comment = input("Type a Comment/Введите коммент: ")
-while True:
-	try:
-		sub_client.comment(message=comment, wikiId=wikiId)
-		print("Comment Sended/Коммент отправлен")
-	except:
-		pass
+
+async def main():
+	client = amino.Client()
+	email = input("Email >> ")
+	password = input("Password >> ")
+	await client.login(email=email, password=password)
+	wikilink = input("Wiki Link >> ")
+	wiki_info = await client.get_from_code(wikilink)
+	wikiId = wiki_info.objectId
+	communityid = wiki_info.path[1:wiki_info.path.index('/')]
+	sub_client = await amino.SubClient(comId=communityid, profile=client.profile)
+	comment = input("Comment >> ")
+	while True:
+		try:
+			await sub_client.comment(message=comment, wikiId=wikiId)
+			print("Comment Sended")
+		except:
+			pass
+
+asyncio.get_event_loop().run_until_complete(main())
