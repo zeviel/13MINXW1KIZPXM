5f6724062814b3b1a5022cbedc6fcada91fb5236
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-09-14 01:13:49 +0600

    Update AminoWikiSpam.py

--- AminoWikiSpam.py
+++ AminoWikiSpam.py
@@ -1,5 +1,4 @@
-import amino
-import asyncio
+import AminoLab
 import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
@@ -7,23 +6,15 @@ print(Fore.GREEN)
 print("""Script by Lil Zevi
 Github : https://github.com/LilZevi""")
 print(pyfiglet.figlet_format("aminowikispam", font="shadow"))
-
-async def main():
-	client = amino.Client()
-	email = input("Email >> ")
-	password = input("Password >> ")
-	await client.login(email=email, password=password)
-	wikilink = input("Wiki Link >> ")
-	wiki_info = await client.get_from_code(wikilink)
-	wikiId = wiki_info.objectId
-	communityid = wiki_info.path[1:wiki_info.path.index('/')]
-	sub_client = await amino.SubClient(comId=communityid, profile=client.profile)
-	comment = input("Comment >> ")
-	while True:
+client = AminoLab.Client()
+email = input("Email >> ")
+password = input("Password >> ")
+client.auth(email=email, password=password)
+comment = input("Comment >> ")
+wiki_info = client.get_from_link(input("Wiki Link >> "))
+wiki_id = wiki_info.object_Id; ndc_Id = wiki_info.ndc_Id
+while True:
 		try:
-			await sub_client.comment(message=comment, wikiId=wikiId)
-			print("Comment Sended")
-		except:
-			pass
-
-asyncio.get_event_loop().run_until_complete(main())
+			client.submit_comment(ndc_Id=ndc_Id, message=comment, wiki_Id=wiki_id)
+			print("Sended Comment")
+		except Exception as e:	print(e)
